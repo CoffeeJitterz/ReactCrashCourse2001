3c03e738e5d77db157b8bf5a4df43e6b6465829e
Josh Millard <josh.millard10@gmail.com>
2021-12-28 09:54:16 -0500

    useEffect fetch added

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,34 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './compontents/Header'
 import Tasks from './compontents/Tasks'
 import AddTask from './compontents/AddTask'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [ tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3, 
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    }
-  ])
+  const [ tasks, setTasks] = useState([])
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = {  id, ...task }
     setTasks([...tasks, newTask])
+  }
+
+  useEffect(() => {
+    const getTasks = async () => {
+      
+    }
+    fetchTasks()
+  }, [])
+
+  //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
   }
 
   //Delete Task
